355323d15ed4fc9f00cf7b3f267b3686d1a026bb
AhmedNazem <aahhaammdd55@gmail.com>
2025-01-20 03:23:41 +0300

    finishing building the dashboard page

--- frontend/src/app/dashboard/CardPurchaseSummary.tsx
+++ frontend/src/app/dashboard/CardPurchaseSummary.tsx
@@ -5,9 +5,6 @@ import React from "react";
 import {
   Area,
   AreaChart,
-  Bar,
-  BarChart,
-  CartesianGrid,
   ResponsiveContainer,
   Tooltip,
   XAxis,
@@ -17,23 +14,27 @@ import {
 const CardPurchaseSummary = () => {
   const { data, isLoading } = useGetDashboardMetricsQuery();
   const purchaseData = data?.purchaseSummary || [];
+
   const lastDataPoint = purchaseData[purchaseData.length - 1] || null;
+
   return (
-    <div className="row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 bg-white flex flex-col justify-between shadow-md rounded-2xl">
+    <div className="flex flex-col justify-between row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 bg-white shadow-md rounded-2xl">
       {isLoading ? (
         <div className="m-5">Loading...</div>
       ) : (
         <>
-          {/* Header */}
+          {/* HEADER */}
           <div>
-            <h2 className="text=lg font-semibold mb-2 px-7 pt-5">
+            <h2 className="text-lg font-semibold mb-2 px-7 pt-5">
               Purchase Summary
             </h2>
+            <hr />
           </div>
-          {/* body */}
+
+          {/* BODY */}
           <div>
-            {/* body header */}
-            <div className=" mb-4 mt-7 px-7">
+            {/* BODY HEADER */}
+            <div className="mb-4 mt-7 px-7">
               <p className="text-xs text-gray-400">Purchased</p>
               <div className="flex items-center">
                 <p className="text-2xl font-bold">
@@ -42,34 +43,30 @@ const CardPurchaseSummary = () => {
                     : "0"}
                 </p>
                 {lastDataPoint && (
-                  <>
-                    <p
-                      className={`text-sm ${
-                        lastDataPoint.changePercentage! >= 0
-                          ? "text-green-500"
-                          : "text-red-500"
-                      } flex ml-3`}
-                    >
-                      {lastDataPoint.changePercentage! >= 0 ? (
-                        <TrendingUp className="size-5 mr-1" />
-                      ) : (
-                        <TrendingDown className="size-5 mr-1" />
-                      )}
-                      {Math.abs(lastDataPoint.changePercentage!)}%
-                    </p>
-                  </>
+                  <p
+                    className={`text-sm ${
+                      lastDataPoint.changePercentage! >= 0
+                        ? "text-green-500"
+                        : "text-red-500"
+                    } flex ml-3`}
+                  >
+                    {lastDataPoint.changePercentage! >= 0 ? (
+                      <TrendingUp className="w-5 h-5 mr-1" />
+                    ) : (
+                      <TrendingDown className="w-5 h-5 mr-1" />
+                    )}
+                    {Math.abs(lastDataPoint.changePercentage!)}%
+                  </p>
                 )}
               </div>
             </div>
-            {/* charts  */}
-            <ResponsiveContainer width="100%" height={350} className="px-7">
+            {/* CHART */}
+            <ResponsiveContainer width="100%" height={200} className="p-2">
               <AreaChart
                 data={purchaseData}
                 margin={{ top: 0, right: 0, left: -50, bottom: 45 }}
               >
-                <CartesianGrid strokeDasharray="" vertical={false} />
                 <XAxis dataKey="date" tick={false} axisLine={false} />
-
                 <YAxis tickLine={false} tick={false} axisLine={false} />
                 <Tooltip
                   formatter={(value: number) => [
