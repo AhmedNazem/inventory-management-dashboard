c60caf74f4e304baa92ff4d6143c046385b207c6
AhmedNazem <aahhaammdd55@gmail.com>
2025-01-21 23:02:50 +0300

    fixing bug in the dark mode

--- frontend/src/app/inventory/page.tsx
+++ frontend/src/app/inventory/page.tsx
@@ -1,8 +1,9 @@
 "use client";
+
 import { useGetProductsQuery } from "@/state/api";
-import React from "react";
-import Header from "../(components)/Header";
+import Header from "@/app/(components)/Header";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
+
 const columns: GridColDef[] = [
   { field: "productId", headerName: "ID", width: 90 },
   { field: "name", headerName: "Product Name", width: 200 },
@@ -22,16 +23,19 @@ const columns: GridColDef[] = [
   },
   {
     field: "stockQuantity",
-    headerName: "Stack Quantity",
-    width: 110,
+    headerName: "Stock Quantity",
+    width: 150,
     type: "number",
   },
 ];
+
 const Inventory = () => {
   const { data: products, isError, isLoading } = useGetProductsQuery();
+
   if (isLoading) {
     return <div className="py-4">Loading...</div>;
   }
+
   if (isError || !products) {
     return (
       <div className="text-center text-red-500 py-4">
@@ -39,6 +43,7 @@ const Inventory = () => {
       </div>
     );
   }
+
   return (
     <div className="flex flex-col">
       <Header name="Inventory" />
@@ -47,7 +52,7 @@ const Inventory = () => {
         columns={columns}
         getRowId={(row) => row.productId}
         checkboxSelection
-        className="bg-white shadow rounded-lg border border-gray-200 mt-5 text-gray-400 "
+        className="bg-white shadow rounded-lg border border-gray-200 mt-5 !text-gray-700"
       />
     </div>
   );
